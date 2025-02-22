e2e8ffcabbbb20a9167b0dd25c80471c82838e13
Ticlext-Altihaf <yusufsekhanalthaf@gmail.com>
2025-02-22 12:40:10 +0700

    update package.json and tsconfig for Deno support and module resolution changes

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
-import { createApp } from './app';
-import { config } from './config';
+import { createApp } from './app.js';
+import { config } from './config/index.js';
 
 const startServer = async () => {
     try {
